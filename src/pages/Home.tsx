ccad0cb771134b8dbc741a8c9c8053ba37e7b4d9
chrisru-dotcom <cris.ru@gmail.com>
2022-12-01 16:27:52 -0500

    added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,17 @@
 import { Button, Container, Card } from "react-bootstrap";
+
 import storeItems from "../data/items.json";
 
 export function Home() {
   return (
     <>
-      <h1>Home Decor </h1>
+      <h1 className="d-flex align-items-center">Home Decor </h1>
+      <div className="ms-2 text-muted d-flex align-items-center">
+        <h6 className=" text-muted ">
+          Welcome to Home Decor! Please visit our store for our currated
+          selection.
+        </h6>
+      </div>
       <Card className="h-100">
         <Card.Img
           variant="top"
