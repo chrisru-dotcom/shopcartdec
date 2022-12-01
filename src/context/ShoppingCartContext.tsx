16e1ffe88a2165061354793ba73632910256721d
chrisru-dotcom <cris.ru@gmail.com>
2022-12-01 13:43:38 -0500

    added cart functions

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, ReactNode, useContext, useState } from "react";
-//import { ShoppingCart } from "../components/ShoppingCart"
+import { ShoppingCart } from "../components/ShoppingCart";
 
 const ShoppingCartContext = createContext({} as ShoppingCartContext);
 
@@ -92,6 +92,7 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
       }}
     >
       {children}
+      <ShoppingCart isOpen={isOpen} />
     </ShoppingCartContext.Provider>
   );
 }
